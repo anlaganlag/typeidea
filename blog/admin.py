63cb7eec5e2372f522ff7b24a0df9214b2edf939
gal <tanjhu@163.com>
2019-12-30 13:24:20 +0800

    models_admin_doned

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,123 @@
+#import xadmin
+#from xadmin.filters import RelatedFieldListFilter
+#from xadmin.filters import manager
+#from xadmin.layout import Row, Fieldset, Container
+#
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from .models import Post, Category, Tag
+#
+#from .adminforms import PostAdminForm
+#from typeidea.base_admin import BaseOwnerAdmin
 
-# Register your models here.
+
+#class PostInline:
+    #form_layout = (
+        #Container(
+            #Row("title", "desc"),
+        #)
+    #)
+    #extra = 1  # 控制额外多几个
+    #model = Post
+#
+
+#@xadmin.sites.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [PostInline, ]
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
+    fields = ('name', 'status', 'is_nav','owner')
+    def save_model(self,request,obj,form,change):
+        obj.owner = request.user
+        return super(CategoryAdmin,self).save_model(request,obj,form,change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = '文章数量'
+
+
+#@xadmin.sites.register(Tag)
+@admin.register(Tag)
+#class TagAdmin(BaseOwnerAdmin):
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self,request,obj,form,change):
+        obj.owner = request.user
+        return super(TagAdmin,self).save_model(request,obj,form,change)
+#
+#
+#class CategoryOwnerFilter(RelatedFieldListFilter):
+#
+    #@classmethod
+    #def test(cls, field, request, params, model, admin_view, field_path):
+        #return field.name == 'category'
+#
+    #def __init__(self, field, request, params, model, model_admin, field_path):
+        #super().__init__(field, request, params, model, model_admin, field_path)
+        ## 重新获取lookup_choices，根据owner过滤
+        #self.lookup_choices = Category.objects.filter(owner=request.user).values_list('id', 'name')
+#
+#
+#manager.register(CategoryOwnerFilter, take_priority=True)
+#
+#
+#@xadmin.sites.register(Post)
+@admin.register(Post)
+#class PostAdmin(BaseOwnerAdmin):
+class PostAdmin(admin.ModelAdmin):
+    ##form = PostAdminForm
+    list_display = [
+        'title', 'category', 'status',
+        'created_time',  'operator'
+    ]
+    #list_display_links = []
+#
+    #list_filter = ['category', ]
+    #search_fields = ['title', 'category__name']
+    #save_on_top = True
+#
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+#
+    #exclude = ['owner']
+    #form_layout = (
+        #Fieldset(
+            #'基础信息',
+            #Row("title", "category"),
+            #'status',
+            #'tag',
+        #),
+    fields= ('category',
+            'title',
+            'desc',
+            'status',
+            'content',
+            'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self,request,obj,form,change):
+        obj.owner = request.user
+        return super(PostAdmin,self).save_model(request,obj,form,change)
+
+     #def get_media(self):
+         # xadmin基于bootstrap，引入会页面样式冲突，仅供参考, 故注释。
+        media = super().get_media()
+        # media.add_js(['https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js'])
+        # media.add_css({
+            # 'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+        # })
+        # return media
--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
     name = models.CharField(max_length=50, verbose_name="名称")
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
-    #owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
+    owner= models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING,null=True)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
@@ -63,7 +63,7 @@ class Post(models.Model):
     content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
     #content_html = models.TextField(verbose_name="正文html代码", blank=True, editable=False)
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
-    #is_md = models.BooleanField(default=False, verbose_name="markdown语法")
+    is_md = models.BooleanField(default=False, verbose_name="markdown语法")
     category = models.ForeignKey(Category, verbose_name="分类", on_delete=models.DO_NOTHING)
     tag = models.ManyToManyField(Tag, verbose_name="标签")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
